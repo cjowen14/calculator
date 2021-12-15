22b09b31c17ee41010f5adf494e1b58723bf6d37
Cory Owen <cjowen14@outlook.com>
2021-12-14 17:03:31 -0700

    Added square option

--- calc.js
+++ calc.js
@@ -38,6 +38,10 @@ reader.question("What would you like to calculate?", function(input){
 		console.log("answer: ", Math.sqrt(num1));
 	}
 
+	if(mathSymbol === "square"){
+		console.log("answer: ", num1 * num1);
+	}
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 
